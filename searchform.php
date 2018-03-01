69c6241593cff1d4f9d43ca5a6922ede28a0122c
br425 <br425@drexel.edu>
2018-03-01 16:20:43 -0500

    beta 3.2.1

--- searchform.php
+++ searchform.php
@@ -5,3 +5,18 @@
         <input type="search" id="s" name="s" value="" />
         <button type="submit" id="searchsubmit" ><?php include 'search-icon.php'; ?></button>
 </form>
+
+
+<script>
+	var searchField = document.getElementById('s');
+	var searchBtn = document.getElementById('searchsubmit');
+
+	searchBtn.onmouseover = function(event) {
+	  if (this != event.currentTarget) { return false; }
+	  // searchField.classlist.addClass
+	}
+	searchBtn.onmouseout = function(event) {
+	  if (this != event.currentTarget) { return false; }
+	  // mouse leave ...
+	}
+</script>
